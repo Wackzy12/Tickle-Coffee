46bb37137a16773a4db1b2b09301964389472238
vince <vince02123@gmail.com>
2024-10-20 01:22:41 +0800

    new layout for drinks and food fix the price for drinks and foods add quantity for americano add functional quantity button for foods and drinks favorite button add favorite button for all drinks and foods functional favorite for americano

--- tickleapp/lib/screens/home_screen.dart
+++ tickleapp/lib/screens/home_screen.dart
@@ -5,6 +5,13 @@ import 'package:tickleapp/noncoffee_screen/matcha.dart';
 import '../drinks_screen/mocha.dart';
 import '../cart_screen/cart_screen.dart';
 
+// Model for Menu Items
+class MenuItemModel {
+  final String name;
+  final String imagePath;
+
+  MenuItemModel({required this.name, required this.imagePath});
+}
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -12,6 +19,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _homeScreenState extends State<HomeScreen> {
+  List<MenuItemModel> favorites = []; // List to hold favorite items
+
+  // List of all items (you can modify this as needed)
+  final List<MenuItemModel> menuItems = [
+    MenuItemModel(name: 'Mocha', imagePath: 'assets/mocha.jpg'),
+    MenuItemModel(name: 'Matcha', imagePath: 'assets/matcha.jpg'),
+    MenuItemModel(name: 'Croissant', imagePath: 'assets/croissant.jpg'),
+    MenuItemModel(name: 'Spaghetti', imagePath: 'assets/spaghetti.jpg'),
+  ];
+
+  // Function to toggle favorites
+  void toggleFavorite(MenuItemModel item) {
+    setState(() {
+      if (favorites.contains(item)) {
+        favorites.remove(item);
+      } else {
+        favorites.add(item);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -59,6 +87,7 @@ class _homeScreenState extends State<HomeScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                // Balance Container
                 Center(
                   child: Container(
                     padding: EdgeInsets.symmetric(vertical: 60, horizontal: 120),
@@ -97,6 +126,7 @@ class _homeScreenState extends State<HomeScreen> {
                   ),
                 ),
                 SizedBox(height: 16),
+                // Popular Menu Section
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -115,17 +145,18 @@ class _homeScreenState extends State<HomeScreen> {
                 SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
                   child: Row(
-                    children: [
-                      MenuItem(imagePath:'assets/mocha.jpg', screen: MochaScreen()),
-                      MenuItem(imagePath: 'assets/matcha.jpg', screen:MatchaScreen(),),
-                      MenuItem(imagePath: 'assets/croissant.jpg', screen: CroissantScreen(),),
-                      MenuItem(imagePath: 'assets/spaghetti.jpg', screen: SpaghettiScreen(),),
-                      //MenuItem(imagePath: 'assets/cinnamon.jpg'),
-                      //MenuItem(imagePath: 'assets/blueberrycheese.jpg'),
-                    ],
+                    children: menuItems.map((item) {
+                      return MenuItem(
+                        imagePath: item.imagePath,
+                        item: item,
+                        onFavoriteToggle: toggleFavorite,
+                        isFavorite: favorites.contains(item),
+                      );
+                    }).toList(),
                   ),
                 ),
                 SizedBox(height: 20),
+                // Special Offers Section
                 Container(
                   padding: EdgeInsets.symmetric(vertical: 12),
                   decoration: BoxDecoration(
@@ -145,7 +176,6 @@ class _homeScreenState extends State<HomeScreen> {
                   ),
                 ),
                 SizedBox(height: 16),
-            
                 Container(
                   height: 500,
                   decoration: BoxDecoration(
@@ -165,21 +195,27 @@ class _homeScreenState extends State<HomeScreen> {
   }
 }
 
-
 class MenuItem extends StatelessWidget {
   final String imagePath;
-  final Widget screen;
+  final MenuItemModel item;
+  final Function(MenuItemModel) onFavoriteToggle;
+  final bool isFavorite;
 
-  const MenuItem({Key? key, required this.imagePath, required this.screen}) : super(key: key);
+  const MenuItem({
+    Key? key,
+    required this.imagePath,
+    required this.item,
+    required this.onFavoriteToggle,
+    required this.isFavorite,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => screen),
-        );
+        // Navigate to the item's screen (if you have one)
+        // You can replace this with the actual screen for the item
+        // Navigator.push(context, MaterialPageRoute(builder: (context) => YourItemScreen(item)));
       },
       child: Container(
         width: 100,
@@ -191,6 +227,21 @@ class MenuItem extends StatelessWidget {
             image: AssetImage(imagePath),
             fit: BoxFit.cover,
           ),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 5,
+              right: 5,
+              child: GestureDetector(
+                onTap: () => onFavoriteToggle(item),
+                child: Icon(
+                  isFavorite ? Icons.favorite : Icons.favorite_border,
+                  color: isFavorite ? Colors.red : Colors.white,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
