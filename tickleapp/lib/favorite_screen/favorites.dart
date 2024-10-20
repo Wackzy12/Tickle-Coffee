73cb9730ff287d1324a3ed5eb87f42dcc4d69d2f
vince <vince02123@gmail.com>
2024-10-21 00:26:19 +0800

    new layout for drinks and food fix the price for drinks and foods add quantity for americano add functional quantity button for foods and drinks favorite button add favorite button for all drinks and foods functional favorite for americano functional favorite button for all foods and drinks we can now see the our selected favorite drink or food in our home screen item from favorites can now be open from the respective screen

--- tickleapp/lib/favorite_screen/favorites.dart
+++ tickleapp/lib/favorite_screen/favorites.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
 import 'favorites_manager.dart';
 
+import '../drinks_screen/mocha.dart';
+import '../drinks_screen/wmocha.dart';
+import '../drinks_screen/americano.dart';
+import '../drinks_screen/latte.dart';
+import '../drinks_screen/caramel.dart';
+
+import '../noncoffee_screen/matcha.dart';
+import '../noncoffee_screen/earlgrey.dart';
+import '../noncoffee_screen/chai.dart';
+import '../noncoffee_screen/hibiscus.dart';
+import '../noncoffee_screen/lemonpeach.dart';
+
+import '../food_screen/pasta_screen/ravioli.dart';
+import '../food_screen/pasta_screen/carbonara.dart';
+import '../food_screen/pasta_screen/spaghetti.dart';
+import '../food_screen/pasta_screen/macaroni.dart';
+import '../food_screen/pasta_screen/lasagna.dart';
+
+import '../food_screen/pastry_screen/croissant.dart';
+import '../food_screen/pastry_screen/chocolat.dart';
+import '../food_screen/pastry_screen/applepie.dart';
+import '../food_screen/pastry_screen/cinnamon.dart';
+import '../food_screen/pastry_screen/cookies.dart';
+
 class FavoritesScreen extends StatefulWidget {
   @override
   _FavoritesScreenState createState() => _FavoritesScreenState();
@@ -12,13 +36,71 @@ class _FavoritesScreenState extends State<FavoritesScreen> {
   @override
   void initState() {
     super.initState();
-    _favoritesFuture = FavoritesManager().getFavorites(); // Initialize the favorites
+    _loadFavorites(); // Initialize the favorites
+  }
+
+  void _loadFavorites() {
+    setState(() {
+      _favoritesFuture = FavoritesManager().getFavorites();
+    });
   }
 
   Future<void> _refreshFavorites() async {
-    setState(() {
-      _favoritesFuture = FavoritesManager().getFavorites(); // Refresh the future
-    });
+    _loadFavorites(); // Refresh the favorites
+  }
+
+  Widget _getFavoriteScreen(String favorite) {
+    // Map favorites to their respective screens
+    switch (favorite) {
+
+      case 'Mocha':
+        return MochaScreen();
+      case 'White Mocha':
+        return WhiteMochaScreen();
+      case 'Caramel':
+        return CaramelScreen();
+      case 'Cafe Latte':
+        return LatteScreen();
+      case 'Americano':
+        return AmericanoScreen();
+
+      case 'Matcha Latte':
+        return MatchaScreen();
+      case 'Earl Grey Tea':
+        return EarlgreyScreen();
+      case 'Hibiscus Tea':
+        return HibiscusScreen();
+      case 'Chai':
+        return ChaiScreen();
+      case 'Lemon Peach Tea':
+        return LemonpeachScreen();
+
+      case 'Spaghetti Bolognese':
+        return SpaghettiScreen();
+      case 'Macaroni And Cheese':
+        return MacaroniScreen();
+      case 'Lasagna':
+        return LasagnaScreen();
+      case 'Ravioli':
+        return RavioliScreen();
+      case 'Carbonara':
+        return CarbonaraScreen();
+
+      case 'Apple Pie':
+        return ApplepieScreen();
+      case 'Cinnamon Roll':
+        return CinnamonScreen();
+      case 'Croissant':
+        return CroissantScreen();
+      case 'Pan eu Chocolat':
+        return ChocolatScreen();
+      case 'Cookies':
+        return CookiesScreen();
+
+    // Add more cases here for other items
+      default:
+        return Scaffold(body: Center(child: Text('Item not found')));
+    }
   }
 
   @override
@@ -41,15 +123,26 @@ class _FavoritesScreenState extends State<FavoritesScreen> {
           return ListView.builder(
             itemCount: favorites.length,
             itemBuilder: (context, index) {
+              final favorite = favorites[index];
               return ListTile(
-                title: Text(favorites[index]),
+                title: Text(favorite),
                 trailing: IconButton(
                   icon: Icon(Icons.remove_circle),
                   onPressed: () async {
-                    await FavoritesManager().removeFavorite(favorites[index]);
+                    await FavoritesManager().removeFavorite(favorite);
                     _refreshFavorites(); // Refresh the UI after removing
                   },
                 ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => _getFavoriteScreen(favorite),
+                    ),
+                  ).then((_) {
+                    _refreshFavorites(); // Refresh favorites when coming back
+                  });
+                },
               );
             },
           );
